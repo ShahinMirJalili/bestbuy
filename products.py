006f50500fe23e9c9dceb540e63fd7d93ca623da
ShahinMirJalili <291028946+ShahinMirJalili@users.noreply.github.com>
2026-06-10 17:48:00 +0200

    Produktklasse erstellt

---
+++ products.py
@@ -0,0 +1,55 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if name == "":
+            raise ValueError("Name darf nicht leer sein")
+        if price < 0:
+            raise ValueError("Price darf nicht negativ sein")
+        if quantity < 0:
+            raise ValueError("Quantity darf nicht negativ sein")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy (self, quantity):
+        if quantity > self.quantity:
+            raise ValueError("Nicht genug auf Lager")
+        self.set_quantity(self.quantity - quantity)
+        return self.price * quantity
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+if __name__ == "__main__":
+    main()
